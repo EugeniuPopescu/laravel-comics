b64dc83212c9276cf97d9b41b61411a94b6df102
Eugen <epopescuu@yahoo.it>
2024-01-19 15:48:49 +0100

    non legge i dati

--- config/data.php
+++ config/data.php
@@ -1,3 +1,46 @@
 <?php
 
-return [];
+return [
+    "links" => [
+        [
+            "text" => "characters",
+            "url" => "#characters",
+        ],
+        [
+            "text" => "comics",
+            "url" => "#comics",
+        ],
+        [
+            "text" => "movies",
+            "url" => "#movies",
+        ],
+        [
+            "text" => "tv",
+            "url" => "#tv",
+        ],
+        [
+            "text" => "games",
+            "url" => "#games",
+        ],
+        [
+            "text" => "collectibles",
+            "url" => "#collectibles",
+        ],
+        [
+            "text" => "videos",
+            "url" => "#videos",
+        ],
+        [
+            "text" => "fans",
+            "url" => "#fans",
+        ],
+        [
+            "text" => "news",
+            "url" => "#news",
+        ],
+        [
+            "text" => "shop",
+            "url" => "#shop",
+        ],
+    ]
+];
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>laravel-comics</h1>
+<header>
+    <div class="row">
+        <!-- col logo -->
+        <div class="col-5">
+            <img class="logo my-3" src="/images/dc-logo.png" alt="">
+        </div>
+    
+        <!-- col links -->
+        <div class="col-7 d-flex justify-content-between align-items-center">
+            @foreach ($links as $link)
+            <a href="">{{ $link }}</a>
+            @endforeach
+        </div>
+    </div>
+</header>
 @endsection
